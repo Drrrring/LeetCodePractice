963876e19eeda3d2661dc048624e8e1e516177e5
Drrrring <y6655533@163.com>
2023-05-19 11:00:32 +0800

    add: Medium/198

---
+++ java/src/Medium/M198.java
@@ -0,0 +1,24 @@
+package Medium;
+
+/**
+ * 198. 打家劫舍
+ * 你是一个专业的小偷，计划偷窃沿街的房屋。每间房内都藏有一定的现金，影响你偷窃的唯一制约因素就是相邻的房屋装有相互连通的防盗系统，
+ * 如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。
+ * 给定一个代表每个房屋存放金额的非负整数数组，计算你 不触动警报装置的情况下 ，一夜之内能够偷窃到的最高金额。
+ */
+public class M198 {
+    // 动态规划，思路同E749
+    public int rob(int[] nums) {
+        if (nums.length == 1) return nums[0];
+        if (nums.length == 2) return Math.max(nums[0], nums[1]);
+        // dp[i] 记录到达i位置时可以获得的最大值，dp[i] = max(dp[i-2], dp[i-3]) + nums[i]
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = nums[1];
+        dp[2] = dp[0] + nums[2];
+        for (int i = 3; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 2], dp[i - 3]) + nums[i];
+        }
+        return Math.max(dp[nums.length - 1], dp[nums.length - 2]);
+    }
+}
